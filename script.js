e2929aea0cccea8e3af212a349f45ac4cbb34047
Neural-Enigma <fcad.td@gmail.com>
2023-08-28 11:34:16 +0300

    Refactor HTML and JS for better code readability

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-var origBoard;
-let huPlayer = 'X';
-let aiPlayer = 'O';
+let originalBoard;
+let humanPlayer = 'X';
+let computerPlayer = 'O';
+let wins = 0;
+let losses = 0;
+let draws = 0;
 
-function chooseSymbol(symbol) {
-    huPlayer = symbol;
-    aiPlayer = symbol === 'X' ? 'O' : 'X';
-    document.getElementById("currentSymbol").innerText = huPlayer;
-    startGame();
-}
-
-const winCombos = [
+const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -18,166 +14,152 @@ const winCombos = [
     [2, 5, 8],
     [0, 4, 8],
     [6, 4, 2]
-]
+];
 
 const cells = document.querySelectorAll('.cell');
+
 startGame();
 
 function startGame() {
-    document.getElementById("currentSymbol").innerText = huPlayer;
-    origBoard = Array.from(Array(9).keys());
-    for (var i = 0; i < cells.length; i++) {
-        cells[i].innerText = '';
-        cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', turnClick, false);
+    document.getElementById("currentSymbol").innerText = humanPlayer;
+    originalBoard = Array.from(Array(9).keys());
+    cells.forEach((cell, index) => {
+        cell.innerText = '';
+        cell.style.removeProperty('background-color');
+        cell.addEventListener('click', turnClick, false);
+    });
+}
+
+function turnClick(event) {
+    const squareId = event.target.id;
+    if (typeof originalBoard[squareId] === 'number') {
+        makeMove(squareId, humanPlayer);
+        if (!checkWin(originalBoard, humanPlayer) && !checkTie()) {
+            makeMove(findBestMove(), computerPlayer);
+        }
     }
 }
 
-function turnClick(square) {
-    if (typeof origBoard[square.target.id] == 'number') {
-        turn(square.target.id, huPlayer)
-        if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+function makeMove(squareId, player) {
+    originalBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    const gameWon = checkWin(originalBoard, player);
+    if (gameWon) {
+        endGame(gameWon);
     }
-}
-
-function turn(squareId, player) {
-    origBoard[squareId] = player;
-    const cell = document.getElementById(squareId);
-    cell.innerText = player;
-    cell.classList.add("fade-in");
-    let gameWon = checkWin(origBoard, player)
-    if (gameWon) gameOver(gameWon)
 }
 
 function checkWin(board, player) {
-    let plays = board.reduce((a, e, i) =>
-        (e === player) ? a.concat(i) : a, []);
+    const playedCells = board.reduce((acc, cell, index) => (cell === player ? [...acc, index] : acc), []);
     let gameWon = null;
-    for (let [index, win] of winCombos.entries()) {
-        if (win.every(elem => plays.indexOf(elem) > -1)) {
-            gameWon = { index: index, player: player };
+
+    for (const [index, win] of winningCombinations.entries()) {
+        if (win.every(cell => playedCells.includes(cell))) {
+            gameWon = { index, player };
             break;
         }
     }
     return gameWon;
 }
 
-function gameOver(gameWon) {
-    for (let index of winCombos[gameWon.index]) {
-        document.getElementById(index).style.backgroundColor =
-            gameWon.player == huPlayer ? "blue" : "red";
+function endGame(gameWon) {
+    for (const index of winningCombinations[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor = gameWon.player === humanPlayer ? 'blue' : 'red';
     }
-    for (var i = 0; i < cells.length; i++) {
-        cells[i].removeEventListener('click', turnClick, false);
-    }
-    updateScoreBoard(gameWon.player == huPlayer ? "win" : "loss");
+    cells.forEach(cell => cell.removeEventListener('click', turnClick, false));
+    updateScoreBoard(gameWon.player === humanPlayer ? 'win' : 'loss');
 }
+
+
+function updateScoreBoard(result) {
+    if (result === 'win') {
+        wins++;
+        document.getElementById('wins').innerText = wins;
+    } else if (result === 'loss') {
+        losses++;
+        document.getElementById('losses').innerText = losses;
+    } else if (result === 'draw') {
+        draws++;
+        document.getElementById('draws').innerText = draws;
+    }
+}
+
 
 function emptySquares() {
-    return origBoard.filter(s => typeof s == 'number');
+    return originalBoard.filter(cell => typeof cell === 'number');
 }
 
-function bestSpot() {
-    return minimax(origBoard, aiPlayer).index;
+function findBestMove() {
+    return minimax(originalBoard, computerPlayer).index;
 }
 
 function checkTie() {
-    if (emptySquares().length == 0) {
-        for (var i = 0; i < cells.length; i++) {
-            cells[i].style.backgroundColor = "green";
-            cells[i].removeEventListener('click', turnClick, false);
-        }
-        updateScoreBoard("draw");
+    if (emptySquares().length === 0) {
+        cells.forEach(cell => {
+            cell.style.backgroundColor = 'green';
+            cell.removeEventListener('click', turnClick, false);
+        });
+        updateScoreBoard('draw');
         return true;
     }
     return false;
 }
 
 function minimax(newBoard, player) {
-    var availSpots = emptySquares();
+    const availableSpots = emptySquares(newBoard);
 
-    if (checkWin(newBoard, huPlayer)) {
+    if (checkWin(newBoard, humanPlayer)) {
         return { score: -10 };
-    } else if (checkWin(newBoard, aiPlayer)) {
+    }
+    if (checkWin(newBoard, computerPlayer)) {
         return { score: 10 };
-    } else if (availSpots.length === 0) {
+    }
+    if (availableSpots.length === 0) {
         return { score: 0 };
     }
 
-    var moves = [];
-    for (var i = 0; i < availSpots.length; i++) {
-        var move = {};
-        move.index = newBoard[availSpots[i]];
-        newBoard[availSpots[i]] = player;
+    const moves = availableSpots.map(spot => {
+        const move = { index: spot };
+        newBoard[spot] = player;
 
-        if (player == aiPlayer) {
-            var result = minimax(newBoard, huPlayer);
-            move.score = result.score;
+        if (player === computerPlayer) {
+            move.score = minimax(newBoard, humanPlayer).score;
         } else {
-            var result = minimax(newBoard, aiPlayer);
-            move.score = result.score;
+            move.score = minimax(newBoard, computerPlayer).score;
         }
-        newBoard[availSpots[i]] = move.index;
-        moves.push(move);
-    }
-    var bestMove;
-    if (player === aiPlayer) {
-        var bestScore = -10000;
-        for (var i = 0; i < moves.length; i++) {
-            if (moves[i].score > bestScore) {
-                bestScore = moves[i].score;
-                bestMove = i;
-            }
-        }
-    } else {
-        var bestScore = 10000;
-        for (var i = 0; i < moves.length; i++) {
-            if (moves[i].score < bestScore) {
-                bestScore = moves[i].score;
-                bestMove = i;
-            }
-        }
-    }
-    return moves[bestMove];
-}
-let wins = 0;
-let losses = 0;
-let draws = 0;
 
-function updateScoreBoard(result) {
-    if (result === 'win') {
-        wins++;
-        document.getElementById("wins").innerText = wins;
-    } else if (result === 'loss') {
-        losses++;
-        document.getElementById("losses").innerText = losses;
-    } else if (result === 'draw') {
-        draws++;
-        document.getElementById("draws").innerText = draws;
-    }
+        newBoard[spot] = move.index;
+        return move;
+    });
+
+    const bestMove = moves.reduce((best, move) => {
+        const condition = player === computerPlayer
+            ? move.score > (best.score || -Infinity)
+            : move.score < (best.score || Infinity);
+        return condition ? move : best;
+    }, {});
+
+    return bestMove;
 }
 
 function toggleSymbol() {
     const isChecked = document.getElementById('symbol-toggle').checked;
-    const currentSymbol = isChecked ? 'O' : 'X';
-    document.getElementById('currentSymbol').innerText = currentSymbol;
-    chooseSymbol(currentSymbol);
+    const newSymbol = isChecked ? 'O' : 'X';
+    document.getElementById('currentSymbol').innerText = newSymbol;
+    chooseSymbol(newSymbol);
 }
 
-
-function toggleSidebar() {
-    const sidebar = document.getElementById("symbolSidebar");
-    if (sidebar.style.width === "0px" || sidebar.style.width === "") {
-        sidebar.style.width = "250px";
-    } else {
-        sidebar.style.width = "0";
-    }
+function chooseSymbol(symbol) {
+    humanPlayer = symbol;
+    computerPlayer = symbol === 'X' ? 'O' : 'X';
+    startGame();
 }
+
 function resetScoreBoard() {
     wins = 0;
     losses = 0;
     draws = 0;
-    document.getElementById("wins").innerText = wins;
-    document.getElementById("losses").innerText = losses;
-    document.getElementById("draws").innerText = draws;
+    document.getElementById('wins').innerText = wins;
+    document.getElementById('losses').innerText = losses;
+    document.getElementById('draws').innerText = draws;
 }
